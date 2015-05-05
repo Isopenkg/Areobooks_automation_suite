b0858160416e062f9033e8d03ffd46039a896af7
Georgiy Isopenko <georgiy.isopenko@techmagic.co>
2015-05-05 11:38:03 +0300

    Creation of first 13 test cases. - Authorisation (Login / Sign Up) - Settings screen.

---
+++ src/test/java/data/SettingsError.java
@@ -0,0 +1,10 @@
+package data;
+
+/**
+ * Created by techmagic on 04.05.15.
+ */
+public class SettingsError {
+
+    String notActivatedAccountError = "We’ve noticed that you have not activated your account. It is important that you activate your account so that you can get the most out of Areobooks. We have sent you an email containing the activation link.";
+
+}
